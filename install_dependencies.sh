232b3941515e8353a5c3e44ede1780570bf2bded
Julian Sia <siajat@utexas.edu>
2014-12-19 02:14:50 -0600

    dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,12 +2,13 @@
 
 # get open cv2.49 for OSX
 wget https://github.com/jayrambhia/Install-OpenCV/blob/master/Ubuntu/2.4/opencv2_4_9.sh
-chmod +x opencv.sh
-./opencv.sh
+chmod +x /opencv2_4_9.sh
+./opencv2_4_9.sh
 
 # get and install PyUserInput
 git clone https://github.com/SavinaRoja/PyUserInput.git 
-python ./PyUserInput/setup.py install
+cd ./PyUserInput
+python setup.py install
 
 
 
