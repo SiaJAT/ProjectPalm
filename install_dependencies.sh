9e92376087854442ee9020c623fa71cff0d81671
Julian Sia <siajat@utexas.edu>
2014-12-19 02:01:31 -0600

    updated build script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-# get homebrew
-ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
-brew doctor
-
 # get open cv2.49 for OSX
-brew tap homebrew/science
-brew install opencv
-mkdir -p ~/Library/Python/2.7/lib/python/site-packages
-echo '/usr/local/lib/python2.7/site-packages' > ~/Library/Python/2.7/lib/python/site-packages/homebrew.pth
+wget https://github.com/jayrambhia/Install-OpenCV/blob/master/Ubuntu/2.4/opencv2_4_9.sh
+chmod +x opencv.sh
+./opencv.sh
 
 # get and install PyUserInput
 git clone https://github.com/SavinaRoja/PyUserInput.git 
-python ./PyUserInput/setup.py
+python ./PyUserInput/setup.py install
 
 
 
